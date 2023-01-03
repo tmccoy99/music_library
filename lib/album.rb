dbe5d5e4a30e6322e6d22bf87d983362656422e4
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-03 16:37:38 +0000

    Implemented equality method

--- lib/album.rb
+++ lib/album.rb
@@ -1,3 +1,9 @@
 class Album
   attr_accessor :title, :release_year, :id, :artist_id
+
+  def ==(other)
+    [self.title, self.release_year, self.id, self.artist_id] == [
+     other.title, other.release_year, other.id, other.artist_id]
+  end
+
 end
