3893f2ad424c85a2d338167552c4341d2f3ee594
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-03 16:37:56 +0000

    Fixed all method

--- lib/album_repository.rb
+++ lib/album_repository.rb
@@ -6,14 +6,16 @@ class AlbumRepository
     sql = "SELECT * FROM albums"
     result_set = DatabaseConnection.exec_params(sql, [])
 
-    result_set.map! do |record|
+    arr = result_set.map do |record|
       album = Album.new
       album.title = record["title"]
-      album.id = record["id"]
-      album.artist_id = record["artist_id"]
-      album.release_year = record["release_year"]
+      album.id = record["id"].to_i
+      album.artist_id = record["artist_id"].to_i
+      album.release_year = record["release_year"].to_i
       album
     end
+
+  arr
   end
 
   def find
