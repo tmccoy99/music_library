77bd69264661b6c69b4f1d40f5359e4299200a8a
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-03 17:06:13 +0000

    Implemented find method

--- lib/album_repository.rb
+++ lib/album_repository.rb
@@ -4,26 +4,33 @@ class AlbumRepository
 
   def all
     sql = "SELECT * FROM albums"
-    result_set = DatabaseConnection.exec_params(sql, [])
-
-    arr = result_set.map do |record|
-      album = Album.new
-      album.title = record["title"]
-      album.id = record["id"].to_i
-      album.artist_id = record["artist_id"].to_i
-      album.release_year = record["release_year"].to_i
-      album
+    query_result = DatabaseConnection.exec_params(sql, [])
+    arr = query_result.map { |record| convert_to_album(record) }
     end
 
   arr
   end
 
-  def find
-
+  def find(id)
+    sql = "SELECT * FROM albums WHERE id = #{id}"
+    query_result = DatabaseConnection.exec_params(sql, [])
+    record = query_result[0]
+    convert_to_album(record)
   end
 
   def create
 
+  end
+
+  private
+
+  def convert_to_album(record)
+    album = Album.new
+    album.title = record["title"]
+    album.id = record["id"].to_i
+    album.artist_id = record["artist_id"].to_i
+    album.release_year = record["release_year"].to_i
+    album
   end
 
 end
