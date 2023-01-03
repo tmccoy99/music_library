4b6771903652b560e87755742b82afe09d3c03b9
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-03 15:52:34 +0000

    Implemented #all method

--- lib/album_repository.rb
+++ lib/album_repository.rb
@@ -1,7 +1,19 @@
+require "database_connection"
+
 class AlbumRepository
 
   def all
+    sql = "SELECT * FROM albums"
+    result_set = DatabaseConnection.exec_params(sql, [])
 
+    result_set.map! do |record|
+      album = Album.new
+      album.title = record["title"]
+      album.id = record["id"]
+      album.artist_id = record["artist_id"]
+      album.release_year = record["release_year"]
+      album
+    end
   end
 
   def find
