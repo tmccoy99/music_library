804702b46f0d1662ac8d9cd92a951d8a730db28e
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-05 15:32:16 +0000

    Implemeted ArtistRepository class

---
+++ lib/artist_repository.rb
@@ -0,0 +1,14 @@
+class ArtistRepository
+
+  def all
+    query = "SELECT * FROM artists"
+    records = DatabaseConnection.exec_params(query, [])
+    records.map do |record|
+      artist = Artist.new
+      artist.name = record["name"]
+      artist.genre = record["genre"]
+      artist.id = record["id"]
+    end
+  end
+
+end
