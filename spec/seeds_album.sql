5dc5373df64c27f7ed5de592e1d4590140527587
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-05 15:47:24 +0000

    Fixed mistype

--- spec/seeds_album.sql
+++ spec/seeds_album.sql
@@ -5,6 +5,6 @@ TRUNCATE TABLE albums RESTART IDENTITY; -- replace with your own table name.
 -- Replace these statements with your own seed data.
 
 INSERT INTO artists (name, genre) VALUES ('Arctic Monkeys', 'Alternative');
-INSERT INTO artists (name, genre) VALUES ('Katy Perry', "Pop");
+INSERT INTO artists (name, genre) VALUES ('Katy Perry', 'Pop');
 INSERT INTO albums (title, release_year, artist_id) VALUES ('AM', 2013, 1);
 INSERT INTO albums (title, release_year, artist_id) VALUES ('Humbug', 2009, 1);
